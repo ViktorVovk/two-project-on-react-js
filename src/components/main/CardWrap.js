85fb1c43f6a83d5d6c87fee3da932bc59bc30c6d
ViktorVovk <34575602+ViktorVovk@users.noreply.github.com>
2018-04-04 18:10:37 +0300

    Update CardWrap.js

--- src/components/main/CardWrap.js
+++ src/components/main/CardWrap.js
@@ -1,29 +1,14 @@
 import React from 'react';
 
 class CardWrap extends React.PureComponent{
-  constructor(props){
-      super(props)
-      this.state={
-        ClassNewProduct: this.props.item.new?"item__new": "new-product hidden",
-        SaleProductClass: this.props.item.discountCost === null?"item__sale hidden":"item__sale",
-        DiscountProductClass: this.props.item.discountCost === null?"price__discount hidden":"price__discount"
-      }
-  }
-  componentWillReceiveProps(nextProps){
-    this.setState({
-      ClassNewProduct: nextProps.item.new?"item__new": "new-product hidden",
-      SaleProductClass: nextProps.item.discountCost === null?"item__sale hidden":"item__sale",
-      DiscountProductClass: nextProps.item.discountCost === null?"price__discount hidden":"price__discount"
-    })
-  }
-
+  
   render(){
     return(
       <article className="products__item">
         <div className="item__img">
           <a href="#"><img src={`.${this.props.item.img}`} alt="Foto Product"/></a>
-          <div className={this.state.SaleProductClass}>sale</div>
-          <div className={this.state.ClassNewProduct}>New</div>
+          <div className={`item__sale  ${this.props.item.discountCost === null?"hidden":""}`}>sale</div>
+          <div className={`item__new ${this.props.item.new?"": "hidden"}`}>New</div>
         </div>
         <div className="item__title">
           <a href="#"><h1>{this.props.item.title}</h1></a>
@@ -35,7 +20,7 @@ class CardWrap extends React.PureComponent{
           <div className="price__coast">
             <p>{`$${this.props.item.cost}.00`}</p>
           </div>
-          <div className={this.state.DiscountProductClass}>
+          <div className={`price__discount ${this.props.item.discountCost === null?"hidden":""}`}>
             <p>{`$${this.props.item.discountCost}.00`}</p>
           </div>
         </div>
